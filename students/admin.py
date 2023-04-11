86cd2674637c505a487668aa2c6e3093d47c6149
plopez26DPU <plopez26@depaul.edu>
2023-04-10 19:05:29 -0500

    initial commit hw9

--- students/admin.py
+++ students/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+
 # Register your models here.
-from .models import Student
+from .models import Student, Course
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Course)
